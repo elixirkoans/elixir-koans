34cb4745693ec75c3c399d3b7f2efa3a2d93b7b9
Uku Taht <uku.taht@gmail.com>
2015-12-17 01:52:34 +0000

    Extract function to get the source file of a module

--- lib/display.ex
+++ lib/display.ex
@@ -1,14 +1,12 @@
 defmodule Display do
   alias IO.ANSI
+  @current_dir File.cwd!
 
   def show_failure(%{expr: expr}, module, name) do
-    source = module.__info__(:compile)
-              |> Dict.get(:source)
-              |> Path.relative_to(File.cwd!)
     IO.puts("")
     IO.puts("Now meditate upon #{display_module(module)}")
     IO.puts("---------------------------------------")
-    IO.puts("Assertion failed in #{source}:#{line_number(expr)}")
+    IO.puts("Assertion failed in #{source_file(module)}:#{line_number(expr)}")
     IO.puts(display_koan(name))
     IO.puts(format_red(Macro.to_string(expr)))
   end
@@ -24,6 +22,12 @@ defmodule Display do
 
   defp line_number({_, [line: line], _}) do
     line
+  end
+
+  defp source_file(module) do
+    module.__info__(:compile)
+    |> Dict.get(:source)
+    |> Path.relative_to(@current_dir)
   end
 
   defp format_red(str) do
