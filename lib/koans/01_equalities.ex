e48cc261a90c0e846814462b4b23cd918a6f24ed
Felipe Sere <felipesere@gmail.com>
2016-03-10 23:21:27 +0000

    Strip out answers from Equalities

--- lib/koans/01_equalities.ex
+++ lib/koans/01_equalities.ex
@@ -2,22 +2,22 @@ defmodule Equalities do
   use Koans
 
   koan "We shall contemplate truth by testing reality, via equality" do
-    assert true == true
+    assert true == :__
   end
 
   koan "To understand reality, we must compare our expectations against reality" do
-    assert 2 == 1 + 1
+    assert 2 == 1 + :__
   end
 
   koan "Some things may appear different, but be the same" do
-    assert 1 == 2 / 2
+    assert 1 == 2 / :__
   end
 
   koan "Something is not equal to nothing" do
-    assert !(1 == nil)
+    assert !(:__ == nil)
   end
 
   koan "When things cannot be equal, they must be different" do
-    refute :something == 4
+    refute :__ == 4
   end
 end
