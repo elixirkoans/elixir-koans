a57ba3da2de95fdaedc7b648fd47cb3d81824647
Mahmut Surekci <msurekci@dctfh.com>
2016-05-25 12:06:52 +0100

    Add koans for floats, rename to numbers koans, reposition on the list

---
+++ test/koans/numbers_koans_test.exs
@@ -0,0 +1,25 @@
+defmodule NumbersTests do
+  use ExUnit.Case
+  import TestHarness
+
+  test "Numbers" do
+    answers = [
+      true,
+      true,
+      [5, 8, 1, 2, 7],
+      1234,
+      '7',
+      "1234",
+      42,
+      35.0,
+      34.3,
+      99.0,
+      12.34,
+      {34.5, ""},
+      1.0,
+      {:multiple, [6.0, 5.0, 8.94, -5.567]},
+    ]
+
+    test_all(Numbers, answers)
+  end
+end
