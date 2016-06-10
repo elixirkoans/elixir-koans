43c2bbdd7187e4fd4c00bc83610f72bff32a5af7
Jay Hayes <ur@iamvery.com>
2016-06-10 11:39:39 -0500

    Show left and right hand values when equality fails
    For assertions that contain function calls, displaying just the
    expression is not helpful. For e.g.
    
      Assertion failed
      String.upcase("foo") == "Foo"
    
    The learner is given no extra information to help them learn. Sure they
    might consult the documentation (and should!), but perhaps a better
    experience would be for us to inform the learner about the values it
    encountered.
    
      Assertion failed
      String.upcase("foo") == "Foo"
      left:  "FOO"
      right: "Foo"
    
    Learner: :bulb: ah hah! it upcases the whole string! :dizzy:

--- lib/display/failure.ex
+++ lib/display/failure.ex
@@ -9,16 +9,28 @@ defmodule Display.Failure do
     #{Paint.red(message)}
     """
   end
-  def format_failure(%{error: %ExUnit.AssertionError{expr: expr}, file: file, line: line}) do
+  def format_failure(%{error: %ExUnit.AssertionError{expr: expr} = error, file: file, line: line}) do
     """
     #{Paint.cyan("Assertion failed in #{file}:#{line}")}
     #{Paint.red(Macro.to_string(expr))}
     """
+    |> format_inequality(error)
   end
   def format_failure(%{error: error, file: file, line: line}) do
     """
     #{Paint.cyan("Error in #{file}:#{line}")}
     #{format_error(error)}
+    """
+  end
+
+  defp format_inequality(message, %{left: @no_value, right: @no_value}) do
+    message
+  end
+  defp format_inequality(message, %{left: left, right: right}) do
+    """
+    #{message}
+    left:  #{left |> inspect |> Paint.yellow}
+    right: #{right |> inspect |> Paint.yellow}
     """
   end
 
