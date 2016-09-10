753502a89b9f71e31950d34df47c397726091fa8
Cliche818 <jeff.chun.yinli@gmail.com>
2016-09-10 16:53:12 -0400

    fixed not reloading files after intellij autosaves

--- lib/watcher.ex
+++ lib/watcher.ex
@@ -13,18 +13,16 @@ defmodule Watcher do
   end
 
   defp reload(file) do
-    if Path.extname(file) == ".ex" do
-      try do
-        file
-        |> normalize
-        |> Code.load_file
-        |> Enum.map(&(elem(&1, 0)))
-        |> Enum.find(&Runner.koan?/1)
-        |> Runner.modules_to_run
-        |> Runner.run
-      rescue
-        e -> Display.show_compile_error(e)
-      end
+    try do
+      file
+      |> normalize
+      |> Code.load_file
+      |> Enum.map(&(elem(&1, 0)))
+      |> Enum.find(&Runner.koan?/1)
+      |> Runner.modules_to_run
+      |> Runner.run
+    rescue
+      e -> Display.show_compile_error(e)
     end
   end
 
