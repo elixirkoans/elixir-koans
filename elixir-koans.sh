f6a32203695049f9a865fe3a8a94a2cef8eefe86
krlsdu <krlsdu@gmail.com>
2018-08-11 01:21:46 -0300

    vim show numbers line.

--- elixir-koans.sh
+++ elixir-koans.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tmux new-session -d 'vim lib/koans/*'
+tmux new-session -d 'vim "+set number" lib/koans/*'
 tmux split-window -v -p 50 'mix meditate'
 tmux select-pane -t 0
 tmux -2 attach-session -d
