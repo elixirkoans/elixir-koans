0f72a9844455cbe94754008df05ee60b6a149d13
Felipe Sere <felipesere@gmail.com>
2019-01-05 11:14:54 +0000

    ljust is being deprecated

--- lib/display/progress_bar.ex
+++ lib/display/progress_bar.ex
@@ -4,7 +4,7 @@ defmodule Display.ProgressBar do
   def progress_bar(%{current: current, total: total}) do
     arrow = calculate_progress(current, total) |> build_arrow
 
-    "|" <> String.ljust(arrow, @progress_bar_length) <> "| #{current} of #{total}"
+    "|" <> String.pad_trailing(arrow, @progress_bar_length) <> "| #{current} of #{total}"
   end
 
   defp calculate_progress(current, total) do
