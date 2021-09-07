cb6ed41a67e56fddb1a57db41b11d4ab24ae1b0f
Michelle <mchllgndrn@gmail.com>
2021-09-07 15:09:55 -0500

    Solved koans 1, 2

--- lib/koans/01_equalities.ex
+++ lib/koans/01_equalities.ex
@@ -10,30 +10,30 @@ defmodule Equalities do
 
   # Replace ___ with the answer to make the koan pass.
   koan "We shall contemplate truth by testing reality, via equality" do
-    assert true == ___
+    assert true == true
   end
 
   koan "Not something is the opposite of it" do
-    assert !true == ___
+    assert !true == false
   end
 
   koan "To understand reality, we must compare our expectations against reality" do
-    assert 2 == 1 + ___
+    assert 2 == 1 + 1
   end
 
   koan "Some things may appear different, but be the same" do
-    assert 1 == 2 / ___
+    assert 1 == 2 / 2
   end
 
   koan "Unless they actually are different" do
-    assert 3.2 != ___
+    assert 3.2 != 3
   end
 
   koan "Some may be looking for bigger things" do
-    assert ___ > 3
+    assert 4 > 3
   end
 
   koan "Others are happy with less" do
-    assert ___ < 3
+    assert 2 < 3
   end
 end
